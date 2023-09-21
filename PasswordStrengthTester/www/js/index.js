e2e0d9903bb732b95aaf9b262852cd718d6d191d
Nathannicolle <nathan-nicolle@orange.fr>
2023-09-21 22:56:58 +0200

    added a system for calculating points according to password length
    between 0 and 8 characters bg is red, between 8 and 10 bg is orange, above the bg is lime
    
    points will be useful when I add special characters and other criterions in my calculation of password complexity

--- PasswordStrengthTester/www/js/index.js
+++ PasswordStrengthTester/www/js/index.js
@@ -1,5 +1,7 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
+let complexityPoints = 0;
+
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 }
@@ -8,10 +10,39 @@ window.addEventListener("load", () => {
     let passwordField = document.querySelector("#password");
 
     passwordField.addEventListener("input", () => {
-        if(passwordField.value.length > 8) {
-            document.querySelector("body").style.background = "lime";
-        } else {
-            document.querySelector("body").style.background = "red";
-        }
+        calculateLengthPoints(passwordField.value);
+        modifyBgColor();
     })
 });
+
+// Fonction pour calculer la complexité selon sa longueur
+function calculateLengthPoints(password) {
+    switch (true) {
+        case (password.length > 8 && password.length <= 10) :
+            complexityPoints++;
+            break;
+        case (password.length > 10) :
+            complexityPoints += password.length / 2;
+            break;
+        default :
+            complexityPoints = 0;
+            break;
+    }
+
+    return complexityPoints;
+}
+
+// Fonction pour modifier la couleur de fond selon la complexité du mdp saisi
+function modifyBgColor() {
+    switch (true) {
+        case (complexityPoints === 1) :
+            document.querySelector("body").style.background = "orange";
+            break;
+        case (complexityPoints > 1) :
+            document.querySelector("body").style.background = "lime";
+            break;
+        default :
+            document.querySelector("body").style.background = "red";
+            break;
+    }
+}
