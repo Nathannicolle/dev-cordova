59d662d55463fa317a7a89c06295f4666b70f61f
Nathannicolle <nathan-nicolle@orange.fr>
2023-09-26 15:40:14 +0200

    show parking name and address successfully
    to do so, for each parking gotten I create a div which contains a title and a paragraph
    
    the title correspond to parking name and the paragraph to its address

--- APItwisto/www/js/index.js
+++ APItwisto/www/js/index.js
@@ -1,3 +1,5 @@
+let parkingList = document.querySelector(".parkingList");
+
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
@@ -9,15 +11,29 @@ window.addEventListener("load", () => {
 })
 
 function init() {
-    let parkingList = document.querySelector(".parkingList");
+    getParkingList();
+}
 
+function getParkingList() {
     // Récupération de la liste des parkings
     fetch("https://data.twisto.fr/api/records/1.0/search/?dataset=parking_relais")
         .then((response) => {
             if(response.ok) {
                 response.json().then((dataArray) => {
                     dataArray.records.forEach((record) => {
-                        console.log(record.fields.nom);
+                        console.log(record)
+
+                        let parkingItem = document.createElement("div");
+                        parkingItem.classList.add("parking");
+                        parkingList.appendChild(parkingItem);
+
+                        let parkingName = document.createElement("h3");
+                        parkingName.innerHTML = record.fields.nom;
+                        parkingItem.appendChild(parkingName);
+
+                        let parkingAddress = document.createElement("p");
+                        parkingAddress.innerHTML = record.fields.adresse + ", " + record.fields.code_postal + " " + record.fields.ville;
+                        parkingItem.appendChild(parkingAddress);
                     })
                 });
             } else {
