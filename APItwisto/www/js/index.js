965cd261c40af8c2e2accb5b2ec7a00cedbe0c47
Nathannicolle <nathan-nicolle@orange.fr>
2023-09-26 15:02:02 +0200

    created request to get parking list
    request is ok !

--- APItwisto/www/js/index.js
+++ APItwisto/www/js/index.js
@@ -1,5 +1,23 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
+    init();
+}
 
+window.addEventListener("load", () => {
+    init();
+})
+
+function init() {
+    let parkingList = document.querySelector(".parkingList");
+
+    // Récupération de la liste des parkings
+    fetch("https://data.twisto.fr/api/records/1.0/search/?dataset=parking_relais")
+        .then(function (response) {
+            if(response.ok) {
+                console.log(response);
+            } else {
+                document.querySelector(".error").innerHTML = "Erreur lors de la requête !";
+            }
+        });
 }
